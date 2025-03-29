2d0b2f3d6e3177460a1fc8cb6d6cefd6831b0fa5
Elena <elena.badyn@icloud.com>
2025-03-29 15:54:00 +0100

    added changes

--- task_4/main.py
+++ task_4/main.py
@@ -1,15 +1,25 @@
 def parse_input(user_input):
+    """
+    Parses user input into a command and arguments.
+    Takes user input as parametr and return a tupple with a command and arguments.
+    """
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
 
 
 def input_error(func):
+    """
+    Decorator for handling errors in user input.
+    It catches ValueError, KeyError, and IndexError and returns
+    appropriate error messages instead of stopping the program.
+    """
+
     def inner(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         except ValueError:
-            return "Give me name and phone please"
+            return "Give me name and phone number please"
         except KeyError:
             return "Enter user name"
         except IndexError:
@@ -20,6 +30,7 @@ def input_error(func):
 
 @input_error
 def add_contact(args, contacts):
+    """Adds a new contact to the contacts dictionary."""
     name, phone = args
     contacts[name] = phone
     return "Contact added."
@@ -27,6 +38,10 @@ def add_contact(args, contacts):
 
 @input_error
 def change_contact(args, contacts):
+    """
+    Updates an existing contact's phone number.
+    If such contact does not founded, the function return error.
+    """
     name, new_phone = args
     if name in contacts:
         contacts[name] = new_phone
@@ -36,6 +51,10 @@ def change_contact(args, contacts):
 
 @input_error
 def show_phone(args, contacts):
+    """
+    Shows the phone number of a contact.
+    If such contact does not founded, the function return error.
+    """
     name = args[0]
     if name in contacts:
         return contacts[name]
@@ -44,19 +63,25 @@ def show_phone(args, contacts):
 
 @input_error
 def show_all(contacts):
+    """Displays all saved contacts.
+    If contacts do not founded, the function return relevant message.
+    """
     if not contacts:
         return "Contacts not found"
     return "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
 
 
 def main():
+    """
+    The main function that runs the assistant bot.
+    """
     contacts = {}
     print("Welcome to the assistant bot!")
     while True:
         user_input = input("Enter a command: ")
         command, *args = parse_input(user_input)
 
-        if command in ["close", "exit"]:
+        if command in ["close", "exit", "bye"]:
             print("Good bye!")
             break
         elif command == "hello":
